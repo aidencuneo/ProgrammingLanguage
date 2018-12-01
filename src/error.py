263d07f2fc6414b2b304e33ebb248c324d1a2d6b
aidencuneo <aidencuneo@gmail.com>
2018-12-02 08:29:38 +1100

    Version 0.1 - Completing error checking
    Error checking has been properly implemented, now all that needs to be
    done with it is to make it recognise different possible errors in the
    code before parsing.

--- src/error.py
+++ src/error.py
@@ -1,4 +1,4 @@
-import lexer
+import lexer, textwrap
 
 
 def error_check(s):
@@ -14,20 +14,35 @@ def error_check(s):
                     scope += 1
                 elif b == '}':
                     scope -= 1
+                if scope < 0:
+                    error_code(4, i+1, [s[i], s[i-1], s[i+1]])
+            if c[0][0] == 'RETURN_STATEMENT':
+                if c[1][0] != 'LINEFEED':
+                    if c[1][0] == 'IDENTIFIER':
+                        pass
+                else:
+                    error_code(3, i+1, [s[i], s[i-1], s[i+1]])
     if scope != 0:
-        error_code(2, len(s))
+        error_code(2, len(s), [s[len(s)-2], s[len(s)-3], s[len(s)-1]])
     exit()
 
 
-def error_code(code, l=0):
+def error_code(code, l, e):
     if code == 0:
         print('Main method not found.')
     elif code == 1:
         print('Not enough arguments for variable creation.')
     elif code == 2:
         print('End of file without completed scope.')
+    elif code == 3:
+        print('Return statement requires at least one argument.')
+    elif code == 4:
+        print('Can not end a loop that does not exist. (Occurs when number of "}" is greater than number of "{")')
     else:
         print('Unknown error occurred.')
-    if l > 0:
-        print('At: Line ' + str(l))
+    if l-1 > 0 and len(e) > 1:
+        print(str(l-1) + ': ' + str(e[1]))
+    print(str(l) + ': ' + str(e[0]))
+    if len(e) > 2:
+        print(str(l+1) + ': ' + str(e[2]))
     exit()
--- src/lexer.py
+++ src/lexer.py
@@ -4,7 +4,85 @@ import error, func, re
 def tokenize_line(line):
     l = []
     for a in line:
-        print(a)
+        lf = comma = False
+        if a.endswith(';') and a != ';':
+            a = a[:-1]
+            lf = True
+        if a.endswith(',') and a != ',':
+            a = a[:-1]
+            comma = True
+        if a.startswith('$'):
+            l.append(['VARIABLE', a])
+        elif a in ['if', 'else', 'elif', 'while', 'for']:
+            l.append(['KEYWORD', a])
+        elif a == 'import':
+            l.append(['IMPORT_STATEMENT', a])
+        elif a == 'raise':
+            l.append(['RAISE_STATEMENT', a])
+        elif a == 'return':
+            l.append(['RETURN_STATEMENT', a])
+        elif a in ['pass', 'break']:
+            l.append(['STATIC_KEYWORD', a])
+        elif a == 'fun':
+            l.append(['FUNCTION_DEFINER', a])
+        elif a == 'class':
+            l.append(['CLASS_DEFINER', a])
+        elif a == 'switch':
+            l.append(['SWITCH_DEFINER', a])
+        elif a == 'case':
+            l.append(['CASE_DEFINER', a])
+        elif a == 'try':
+            l.append(['TRY_DEFINER', a])
+        elif a == 'catch':
+            l.append(['CATCH_DEFINER', a])
+        elif a == '?':
+            l.append(['IF_SHORTHAND', a])
+        elif a == '|':
+            l.append(['PIPE', a])
+        elif a == '+=' or a == '-=' or a == '*=' or a == '/=' or a == '%=':
+            l.append(['INCREMENTAL_OPERATOR', a])
+        elif a in '*-/+%=':
+            l.append(['OPERATOR', a])
+        elif a == '&&' or a == '||':
+            l.append(['BINARY_OPERATOR', a])
+        elif a == '==' or a == '!=' or a == '>' or a == '<' or a == '<=' or a == '>=':
+            l.append(['COMPARISON_OPERATOR', a])
+        elif a.startswith('!'):
+            l.append(['FUNCTION_CALL', a])
+        elif a == '{':
+            l.append(['SCOPE+1', a])
+        elif a == '}':
+            l.append(['SCOPE-1', a])
+        elif a == '(':
+            l.append(['SWITCH_BEGIN', a])
+        elif a == ')':
+            l.append(['SWITCH_END', a])
+        elif a == '**':
+            l.append(['COMMENT_DEFINER', a])
+        elif a == '::':
+            l.append(['KEY', a])
+        elif a == ';':
+            l.append(['LINEFEED', ';'])
+        elif func.is_int(a):
+            l.append(['INTEGER', a])
+        elif func.is_float(a):
+            l.append(['FLOAT', a])
+        elif (a.startswith('"') and a.endswith('"')) or (a.startswith("'") and a.endswith("'")):
+            l.append(['STRING', a])
+        elif a.startswith('[') and a.endswith(']'):
+            l.append(['LIST', a])
+        elif a.startswith('`') and a.endswith('`'):
+            l.append(['INCLUDE_STATEMENT', a])
+        elif re.match("[a-z]", a) or re.match("[A-Z]", a):
+            l.append(['IDENTIFIER', a])
+        elif a.startswith('__') and a.endswith('__'):
+            l.append(['SPECIAL_IDENTIFIER', a])
+        else:
+            l.append(['UNKNOWN', a])
+        if lf:
+            l.append(['LINEFEED', ';'])
+        elif comma:
+            l.append(['SYMBOL', ','])
     return l
 
 
